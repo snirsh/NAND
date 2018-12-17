f47ea7cf74bdba661535f2c7dd8622cee209a968
snirsh <s.sharristh@gmail.com>
2018-12-17 10:48:03 +0200

    finished

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -1,13 +1,17 @@
 from CompilationEngine import *
-import ntpath
 import sys
-import os
+import glob
+from os.path import join as osjoin
+
+
+def main():
+    input_path = sys.argv[1]
+    files = [_ for _ in glob.glob(osjoin(input_path, '*' + '.jack'))]
+    for input_file in files:
+        output_name = input_file.split('.jack')[0]
+        output_name += '.xml'
+        CompilationEngine(input_file, output_name)
+
 
 if __name__ == '__main__':
-    input_path = sys.argv[1]
-    files = [file.rstrip() for file in os.listdir(input_path) if file.endswith('.jack')]
-    for file in files:
-        output_name = str(ntpath.basename(file).split('.jack')[0]) + '.xml'
-        output_name = os.path.join(os.path.abspath(input_path), output_name)
-        input_file = os.path.join(os.path.abspath(input_path), file)
-        CompilationEngine(input_file, output_name)
+    main()
