f655d44c33d315c8952196c8b9ed226c3f142100
snirsh <s.sharristh@gmail.com>
2018-12-17 10:01:35 +0200

    finished. working on make runner and README

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -5,7 +5,7 @@ import os
 
 if __name__ == '__main__':
     input_path = sys.argv[1]
-    files = [file for file in os.listdir(input_path) if file.endswith('.jack')]
+    files = [file.rstrip() for file in os.listdir(input_path) if file.endswith('.jack')]
     for file in files:
         output_name = str(ntpath.basename(file).split('.jack')[0]) + '.xml'
         output_name = os.path.join(os.path.abspath(input_path), output_name)
